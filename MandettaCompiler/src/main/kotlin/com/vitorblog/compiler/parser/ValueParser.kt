5eab7bf35168229e4fdf7e15cdbbd858ac6d3407
vitor <vitorblog12@gmail.com>
2020-04-07 12:19:08 -0300

    📦 Adding string Concatenation

--- MandettaCompiler/src/main/kotlin/com/vitorblog/compiler/parser/ValueParser.kt
+++ MandettaCompiler/src/main/kotlin/com/vitorblog/compiler/parser/ValueParser.kt
@@ -1,13 +1,30 @@
 package com.vitorblog.compiler.parser
 
+import com.vitorblog.compiler.dao.VariableDao
 import com.vitorblog.compiler.util.StringUtils
 import java.lang.Exception
+import java.util.regex.Pattern
 
 object ValueParser {
 
     fun parseValue(string: String):Any? {
         when {
             StringUtils.isString(string) -> {
+                val pattern = Pattern.compile("\\{(.*?)\\}")
+                val matcher = pattern.matcher(string)
+                if (matcher.find()){
+                    var newString = string
+
+                    for (match in 0 until matcher.groupCount()){
+
+                        val matchString = matcher.group(match)
+                        newString = newString.replace(matchString, VariableDao[matchString.substring(1, matchString.length-1)]!!.value.toString())
+
+                    }
+
+                    return newString.substring(1, newString.length-1)
+                }
+
                 return string.substring(1, string.length-1)
             }
             StringUtils.isBoolean(string) -> {
