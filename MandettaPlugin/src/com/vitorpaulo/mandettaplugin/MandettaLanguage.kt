f343ad85e04c4a1b1e9cecfe0db529463a8a77f4
vitor <vitorblog12@gmail.com>
2020-07-10 15:43:12 -0300

    🖼 Adding icon

---
+++ MandettaPlugin/src/com/vitorpaulo/mandettaplugin/MandettaLanguage.kt
@@ -0,0 +1,10 @@
+package com.vitorpaulo.mandettaplugin
+
+import com.intellij.lang.Language
+import com.intellij.openapi.util.IconLoader
+
+object MandettaLanguage : Language("Mandetta") {
+
+    val icon = IconLoader.findIcon("icon.png")
+
+}
